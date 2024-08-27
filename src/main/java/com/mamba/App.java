813becc7c728181ed2d242fb972b9cfa8959226f
qiangt <guangtao.qian@china.nttdata.com>
2024-08-27 19:23:12 +0800

    json格式调整

--- src/main/java/com/mamba/App.java
+++ src/main/java/com/mamba/App.java
@@ -7,6 +7,8 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.parser.Feature;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Hello world!
@@ -14,11 +16,22 @@ import java.io.InputStream;
  */
 public class App
 {
+    static List<String> tables = Arrays.asList("base_user"
+            ,"rdwh_project_main"
+            ,"rdwh_bd_virtual_organize"
+            ,"rdwh_bd_virtual_organize_mapping"
+            ,"rdwh_work"
+            ,"rdwh_work_project_2024"
+            ,"rdwh_work_project_info_2024"
+            ,"rdwh_work_content"
+            ,"base_user_extension");
     public static void main( String[] args )
     {
         ClassPathResource cpr = new ClassPathResource("table.json");
         final Object parse = JSON.parse(cpr.readBytes(), Feature.SupportArrayToBean);
         String s = JSON.toJSONString(parse);
+        JSONArray jsonArray = JSONArray.parseArray(s);
+
         System.out.println(s);
     }
 }
